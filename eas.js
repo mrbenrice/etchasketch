9188032fc340561b970e6373483ccaab30d2895c
mrbenrice <111289418+mrbenrice@users.noreply.github.com>
2022-11-24 17:39:42 +0000

    js file mostly finished, others need updated
    index.html will inevitably have some div updating, when I
    start playing with the css file a bit more.
    
    functions finalised in the js file, I used my own knowledge,
    some of google, and a couple of function styles from the some
    of the submissions. This was challenging to put together, but
    I finally feel confident writing & placing functions.
    
    css file will be updated more tomorrow/in the coming days, to
    beautify the website before I add a live preview & add it to me
    portfolio.

---
+++ eas.js
@@ -0,0 +1,60 @@
+let color = 'black';
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+const griddrag = document.querySelector('.grid')
+griddrag.ondragstart = () => {
+    return false;
+};
+
+function setGridSize (cellSize) {
+    let grid = document.querySelector('.grid')
+    let cells = grid.querySelectorAll('div')
+    cells.forEach((div) => div.remove());
+    grid.style.gridTemplateColumns = `repeat(${cellSize}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${cellSize}, 1fr)`;
+
+for (let i = 0; i < (cellSize * cellSize); i++) {
+    let cell = document.createElement('div');
+    cell.addEventListener('mousedown', colorCell)
+    cell.addEventListener('mouseover', colorCell)
+    cell.style.backgroundColor = 'white';
+    grid.insertAdjacentElement("beforeend", cell);
+ }
+}
+
+setGridSize(16);
+
+function changeGridSize (input) {
+    if (input >= 2 && input <= 65)
+    {
+        setGridSize(input);
+        console.log('Correct')
+    } else {
+        console.log('Error');
+    }
+
+}
+
+function colorCell() {
+    if (mouseDown) {
+    if (color == 'random') {
+        this.style.transition = '0.2s';
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`
+    } else {
+        this.style.transition = '0.2s';
+        this.style.backgroundColor = color;
+    }
+}
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetGrid() {
+    let grid = document.querySelector('.grid')
+    let cells = grid.querySelectorAll('div')
+    cells.forEach((div) => div.style.backgroundColor = 'white');
+}
